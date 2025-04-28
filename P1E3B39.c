c750f7f3bd3066d579e01d64ff70f55a1a455c7c
ken <ken930526@gmil.com>
2025-04-29 02:34:20 +0800

    第二部分

--- P1E3B39.c
+++ P1E3B39.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>    
 #include <string.h>
 
+#define PASSWORD "2025"
+void flush_input(void);
+
 int main() {
     
     printf("********************************************\n");
@@ -29,4 +32,29 @@ int main() {
     system("PAUSE");
     //system("clear");
     system("CLS");
+
+    char input[5];
+    int wrongCount=0;
+    while (1){
+        if (wrongCount < 3){
+            printf("請輸入四位數字密碼：");
+            scanf("%4s", input);
+            flush_input();
+            if (strcmp(input, PASSWORD) == 0) {
+                printf("密碼正確，歡迎進入！\n");
+                break;
+            }else {
+                wrongCount++;
+                printf("密碼錯誤，還有 %d 次輸入機會！\n", 3-wrongCount);
+            }
+        }else {
+            printf("密碼錯誤 %d 次，程式結束！\n", wrongCount);
+            return 0;
+        }
+    }
+}
+void flush_input(void) {
+    //int ch;
+    //while ((ch = getchar()) != '\n' && ch != EOF) {}
+    fflush(stdin);
 }
