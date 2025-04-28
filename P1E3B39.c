1371c6584333ffaa234027f6299036950a66e621
ken <ken930526@gmil.com>
2025-04-29 02:29:54 +0800

    第一部分

---
+++ P1E3B39.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>    
+#include <string.h>
+
+int main() {
+    
+    printf("********************************************\n");
+    printf("1     .----------.       \n");
+    printf("2   .'            `.     \n");
+    printf("3  /   .--------. r  \\    \n");
+    printf("4 ;   /          \\   ;   \n");
+    printf("5 |  ;            ;  |   \n");
+    printf("6 ;   \\          /   ;   \n");
+    printf("7  \\   `--------'   /    \n");
+    printf("8   `.            .'     \n");
+    printf("9     `-........-'       \n");
+    printf("10                       \n");
+    printf("11                       \n");
+    printf("12      ( o.o )          \n");
+    printf("13                       \n");
+    printf("14      ( owo )          \n");
+    printf("15                       \n");
+    printf("16      ( ^_^ )          \n");
+    printf("17                       \n");
+    printf("18                       \n");
+    printf("********************************************\n");
+    printf("請按 Enter 繼續...\n");
+    //getchar();
+    system("PAUSE");
+    //system("clear");
+    system("CLS");
+}
